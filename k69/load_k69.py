3a8cf5eb070c7a08b592c678c49e81c67754069c
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-11-26 10:52:15 -0500

    Add docstrings to k69 loader

--- k69/load_k69.py
+++ k69/load_k69.py
@@ -1,6 +1,7 @@
 import numpy
- 
+
 def load_dataset():
+    """Loads the original data as sent to me via email."""
     npzfile = numpy.load('k69_dataset.npz')
     #u_kln matrix
     u_kln = npzfile['u']
@@ -14,5 +15,6 @@ def load_dataset():
 u_kln, N_k, f_k = load_dataset()
 import pymbar
 
+# Now we save a u_kn representation of the dataset using pytables to compress the data to make it fit on Github
 mbar = pymbar.MBAR(u_kln, N_k)
 pymbar.testsystems.pymbar_datasets.save("k69", mbar.u_kn, mbar.N_k, least_significant_digit=8)
